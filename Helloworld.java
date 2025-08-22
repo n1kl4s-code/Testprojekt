a0d4752aa64f7a04196fd81f89b510a50ae29ae8
Alireza <alireza.ebrahimi@edeka.de>
2025-08-22 14:30:21 +0200

    Feierabend

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+package Testprojekt;
+
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, ich bin Alireza");
+        System.out.println("Ich bin 19 Jahre alt");
+        System.out.println("Ich bin ein Student an der Hochschule Augsburg");  
+        System.out.println("Ich studiere Informatik");
+        System.out.println("Ich bin ein großer Fan von Java");      
+        System.out.println("Ich liebe es, Programme zu schreiben");
+        System.out.println("Ich freue mich auf die Zukunft der Informatik");   
+    }
+}   
